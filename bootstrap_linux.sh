c3ec4e9d11227e1ec8d2b382ed218c09c0b1a375
Andrew Jones <andy.larrymite@gmail.com>
2015-07-21 09:49:15 +1000

    fixed problems and updated with java bits

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -5,14 +5,21 @@ echo "#my custom stuff" >> ~/.bashrc
 echo "source ~/.emacs.d/.bashrc" >> ~/.bashrc
 
 ln -s ~/.emacs.d/.emacs .emacs
-
+# build emacs
 sudo apt-get install build-essential
 sudo apt-get build-dep emacs24
-
-# build emacs
 cd ~/Downloads
 wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+tar -xzvf emacs-24.5.tar.gz
 cd emacs-24.5
 ./configure
 make
 sudo make install
+
+# install java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# 32bit libs for android-sdk
+sudo apt-get install lib32z1 lib32ncurses5 lib32stdc++6
