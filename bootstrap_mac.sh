8973b1600e94d601f3b20e9e2222c1a7f630d976
Andrew Jones <andy.larrymite@gmail.com>
2016-03-09 12:01:22 +1100

    added brew commands

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -4,3 +4,7 @@ git clone https://github.com/andii1701/emacs.d-24.git .emacs.d
 
 ln -s ~/.emacs.d/.bash_profile .bash_profile
 ln -s ~/.emacs.d/.emacs .emacs
+
+sudo xcodebuild -license
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
